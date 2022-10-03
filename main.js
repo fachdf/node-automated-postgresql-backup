4c24601fd6baef5265f814112b3e2eea9001aa4f
fachdf <fachridhia.f@gmail.com>
2022-10-03 10:14:03 +0700

    Add Scheduler for Backup

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const {execute} = require('@getvim/execute');
 const dotenv = require('dotenv');
+const cron = require('node-cron');
 dotenv.config();
 const username = process.env.DB_USERNAME;
 const database = process.env.DB_NAME;
 const date = new Date();
 const currentDate = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
-const fileName = `database-backup-${currentDate}.tar`;
+const fileName = `database-backup-${currentDate}.sql`;
 
-function backup() {
+
+
+async function backup() {
     execute(`pg_dump -U ${username} -d ${database} -f ${fileName} -F t`,).then(async () => {
         console.log("Finito");
+        return 0;
     }).catch(err => {
         console.log(err);
     })
 }
 
-backup();
+const task = cron.schedule('59 23 * * FRI', function() {
+    backup().then(console.log('success backup db: ' + database));
+  });
+
+task.start();
